dfcd414a09b440244ae59c91f44870a2250f4669
Hoang Phuong Thao <hoangphuong.thao@blindspot.ai>
2020-03-02 17:00:21 +0100

    Uploading an image and getting it on the backend

--- app.py
+++ app.py
@@ -1,12 +1,37 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, url_for, flash, request, redirect
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = '/Users/Hiro_hankamura/Desktop/HomeProjects/BrusselsAirport/BrusselsAirportBackend/images'
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['SECRET_KEY'] = 'pXuUdktb5IAHe_xbzqEiCA'
 
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html')
 
-@app.route('/')
-def hello_world():
-    return render_template('index.html')
-
+@app.route("/upload_image", methods=['GET', 'POST'])
+def upload_image():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and file.filename.endswith(('.png', '.jpg', '.jpeg')):
+            filename = secure_filename(file.filename)
+            flash('file {} saved'.format(file.filename))
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('upload_image'))
+    return render_template('upload_image.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
