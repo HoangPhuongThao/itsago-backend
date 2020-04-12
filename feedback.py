2cb4cdeb14a6f13f057b54f00048c79d91fd090f
joren.sjongers <joren.sjongers@icloud.com>
2020-04-12 22:31:32 +0200

    Make new folder on every call to avoid crash when folder is deleted

--- feedback.py
+++ feedback.py
@@ -1,18 +1,29 @@
 from pathlib import Path
-Path("feedback").mkdir(parents=True, exist_ok=True)
+
 
 def process_feedback(text):
+    Path("feedback").mkdir(parents=True, exist_ok=True)
     with open("feedback/feedback.txt", 'a+') as file:
         file.write('==============================================\n')
         file.write(text)
         file.write('\n\n')
 
 def process_unfound_item(text):
+    Path("feedback").mkdir(parents=True, exist_ok=True)
     with open("feedback/suggested_items.txt", 'a+') as file:
         file.write('==============================================\n')
         file.write(text)
         file.write('\n\n')
 
+def process_happy_feedback():
+    Path("feedback").mkdir(parents=True, exist_ok=True)
+    with open("feedback/happy.txt", 'a+') as file:
+        file.write('happy\n')
+
+def process_sad_feedback():
+    Path("feedback").mkdir(parents=True, exist_ok=True)
+    with open("feedback/sad.txt", 'a+') as file:
+        file.write('sad\n')
 
 
 
