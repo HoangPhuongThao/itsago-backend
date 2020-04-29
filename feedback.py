a4b422688cf80b8c05cc337dbb859ab617333b37
Hoang Phuong Thao <hoangphuong.thao@blindspot.ai>
2020-04-29 15:30:25 +0200

    Set limit to 8000 requests

--- feedback.py
+++ feedback.py
@@ -1,6 +1,27 @@
 import sqlite3
 import json
 
+# feedback.db consists of 3 tables: requests (where current number of requests to GAPI is hold)
+#                                   rank_feedback (users ranking our app on scale 1-5)
+#                                   notfound_feedback (items that were not found in items.db and are requested by users)
+
+
+def update_nRequests():
+    conn = sqlite3.connect('feedback.db')
+    c = conn.cursor()
+    with conn:
+        c.execute("SELECT * FROM '{}'".format('requests'))
+        current = c.fetchall()[0][0]
+        c.execute('''UPDATE requests SET n_requests = ? WHERE n_requests = ?''', (current + 1, current))
+
+
+def get_nRequests():
+    conn = sqlite3.connect('feedback.db')
+    c = conn.cursor()
+    with conn:
+        c.execute("SELECT * FROM '{}'".format('requests'))
+        return c.fetchall()[0][0]
+
 
 def process_feedback(rank, text):
     conn = sqlite3.connect('feedback.db')
@@ -8,11 +29,13 @@ def process_feedback(rank, text):
     with conn:
         c.execute("INSERT INTO rank_feedback VALUES (:rank, :text)", {'rank': rank, 'text': text})
 
+
 def process_unfound_item(text):
     conn = sqlite3.connect('feedback.db')
     c = conn.cursor()
     with conn:
         c.execute("INSERT INTO notfound_feedback VALUES (:text)", {'text': text})
+
 
 def retrieve_all(table):
     conn = sqlite3.connect('feedback.db')
@@ -20,6 +43,7 @@ def retrieve_all(table):
     c.execute("SELECT * FROM '{}'".format(table))
     data = c.fetchall();
     return json.dumps(data)
+
 
 def remove(table):
     conn = sqlite3.connect('feedback.db')
